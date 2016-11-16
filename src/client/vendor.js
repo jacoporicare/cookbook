60d930fd1e989740db23f1a59b86cc6b5d86fd9d
Jakub Ricar <jakub.ricar@hotmail.com>
2016-11-17 00:39:36 +0100

    Add Vendor modules to vendor.js

--- src/client/vendor.js
+++ src/client/vendor.js
@@ -2,3 +2,9 @@ import './vendor.scss';
 
 import 'react';
 import 'react-dom';
+import 'react-redux';
+import 'react-router';
+import 'react-router-redux';
+import 'react-router-scroll';
+import 'redux';
+import 'redux-thunk';
