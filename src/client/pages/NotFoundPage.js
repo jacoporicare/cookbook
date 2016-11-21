0e375c14e3e418c587d7cc77b22ed6387ab67303
Jakub Ricar <jakub.ricar@hotmail.com>
2016-11-21 21:38:19 +0100

    Page header style on Not Found page

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFoundPage = () => {
   return (
     <div className="container">
-      <h1>Nenalezeno</h1>
+      <h1 className="page-header">Nenalezeno</h1>
       <div className="alert alert-info">
         Toto není stránka, kterou hledáš.
       </div>
