80ee0b0180b8d596dce4d4a95b58df8f4707186f
Jakub Ricar <ricar@cngroup.dk>
2016-11-22 16:33:26 +0100

    Show Spinner after a short delay

--- src/client/components/Spinner/Spinner.js
+++ src/client/components/Spinner/Spinner.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import './Spinner.scss';
 
-const Spinner = () => {
-  return (
-    <div className="spinner">
-      <div className="rect1" />{' '}
-      <div className="rect2" />{' '}
-      <div className="rect3" />{' '}
-      <div className="rect4" />{' '}
-      <div className="rect5" />
-    </div>
-  );
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: (props.delay < 1)
+    };
+  }
+
+  componentDidMount() {
+    if (!this.state.visible) {
+      this.timer = setTimeout(() => this.setState({ visible: true }), this.props.delay || 100);
+    }
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    if (!this.state.visible) {
+      return null;
+    }
+
+    return (
+      <div className="spinner">
+        <div className="rect1" />{' '}
+        <div className="rect2" />{' '}
+        <div className="rect3" />{' '}
+        <div className="rect4" />{' '}
+        <div className="rect5" />
+      </div>
+    );
+  }
+}
+
+Spinner.propTypes = {
+  delay: PropTypes.number
 };
 
 export default Spinner;
