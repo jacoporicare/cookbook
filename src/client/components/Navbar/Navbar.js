175dea6e29e2662a2db7d3b3f4796f302e394e74
Jakub Ricar <ricar@cngroup.dk>
2017-01-07 18:20:10 +0100

    Navbar collapse fix

--- src/client/components/Navbar/Navbar.js
+++ src/client/components/Navbar/Navbar.js
@@ -3,45 +3,64 @@ import { Link, IndexLink } from 'react-router';
 import NavItem from '../NavItem/NavItem';
 // import SearchBar from '../SearchBar/SearchBar';
 
-const Navbar = ({ userName, isAuthenticated, isFetchingUser }) => (
-  <div className="navbar navbar-default">
-    <div className="container">
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <div className="navbar-header">
-        <button className="navbar-toggle" type="button">
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <IndexLink to="/" className="navbar-brand">Žrádelník</IndexLink>
+    this.state = {
+      collapsed: true,
+    };
+  }
+
+  handleCollapseToggle = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  }
+
+  render() {
+    const { userName, isAuthenticated, isFetchingUser } = this.props;
+    const { collapsed } = this.state;
+
+    return (
+      <div className="navbar navbar-default">
+        <div className="container">
+
+          <div className="navbar-header">
+            <button className="navbar-toggle" type="button" onClick={this.handleCollapseToggle}>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <IndexLink to="/" className="navbar-brand">Žrádelník</IndexLink>
+          </div>
+
+          <div className={`navbar-collapse ${collapsed ? 'collapse' : ''}`} id="navbar-main">
+            {isAuthenticated &&
+              <ul className="nav navbar-nav">
+                <NavItem to="/prilohy">Přílohy</NavItem>
+              </ul>
+            }
+
+            {/* isAuthenticated && <SearchBar /> */}
+
+            {isAuthenticated &&
+              <ul className="nav navbar-nav navbar-right">
+                <li><a>{isFetchingUser ? <i className="fa fa-spin fa-spinner" /> : <span><i className="fa fa-user" /> {userName}</span>}</a></li>
+                <li><Link to="/odhlaseni">Odhlásit</Link></li>
+              </ul>
+            }
+
+          </div>
+
+        </div>
       </div>
-
-      <div className="navbar-collapse collapse" id="navbar-main">
-        {isAuthenticated &&
-          <ul className="nav navbar-nav">
-            <NavItem to="/prilohy">Přílohy</NavItem>
-          </ul>
-        }
-
-        {/* isAuthenticated && <SearchBar /> */}
-
-        {isAuthenticated &&
-          <ul className="nav navbar-nav navbar-right">
-            <li><a>{isFetchingUser ? <i className="fa fa-spin fa-spinner" /> : <span><i className="fa fa-user" /> {userName}</span>}</a></li>
-            <li><Link to="/odhlaseni">Odhlásit</Link></li>
-          </ul>
-        }
-
-      </div>
-
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 Navbar.propTypes = {
   userName: PropTypes.string,
   isAuthenticated: PropTypes.bool,
-  isFetchingUser: PropTypes.bool
+  isFetchingUser: PropTypes.bool,
 };
 
 export default Navbar;
