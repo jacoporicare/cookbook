1b843a107e4e960dd290fb906f66e6ead0569c93
Jakub Ricar <ricar@cngroup.dk>
2017-02-11 11:27:13 +0100

    Nginx HTTPS

--- docker/host/proxy-setup.sh
+++ docker/host/proxy-setup.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/certs
+
 docker network create web
-docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro \
-    -v ~/nginx.conf:/etc/nginx/conf.d/nginx.conf:ro \
-    --name nginx-proxy --network web --restart always jwilder/nginx-proxy
+
+docker run -d -p 80:80 -p 443:443 \
+  -v ~/certs:/etc/nginx/certs:ro \
+  -v /etc/nginx/vhost.d \
+  -v /usr/share/nginx/html \
+  -v /var/run/docker.sock:/tmp/docker.sock:ro \
+  -v ~/nginx.conf:/etc/nginx/conf.d/nginx.conf:ro \
+  --name nginx-proxy \
+  --network web \
+  --restart always \
+  jwilder/nginx-proxy
+
+docker run -d \
+  -v ~/certs:/etc/nginx/certs:rw \
+  --volumes-from nginx-proxy \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  --name nginx-proxy-letsencrypt \
+  --network web \
+  --restart always \
+  jrcs/letsencrypt-nginx-proxy-companion
