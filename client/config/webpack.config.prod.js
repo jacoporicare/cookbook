993f97e9985916201b3c60264863d58c12d880d3
Jakub Řičař <jakub.ricar@gmail.com>
2017-12-24 00:06:33 +0100

    Webpack - add ModuleConcatenationPlugin

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -213,6 +213,7 @@ module.exports = {
         windows: false,
       },
     }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
