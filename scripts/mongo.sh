4e069d88c55f7ce1b39270e18b0b5632915bf99d
Jakub Řičař <jakub.ricar@gmail.com>
2018-02-04 00:06:24 +0100

    Update mongo script

--- scripts/mongo.sh
+++ scripts/mongo.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 CONTAINER_NAME=mongo
+DATA_SRC=~/dev/private/mongo_data
+DATA_DST=/data/db
 
-if [[ -n $(docker ps -a | grep -E "Exited.*$CONTAINER_NAME$") ]]; then
-  docker start $CONTAINER_NAME
-elif [[ -z $(docker ps | grep "$CONTAINER_NAME$") ]]; then
-  docker run -d \
-    --name $CONTAINER_NAME \
-    -p 27017:27017 \
-    -v ~/dev/private/mongo_data:/data/db \
-    mongo:3.4
-fi
+docker start $CONTAINER_NAME 1>/dev/null 2>&1 ||
+docker run -d \
+	--name $CONTAINER_NAME \
+	-p 27017:27017 \
+	--mount type=bind,src=$DATA_SRC,dst=$DATA_DST \
+	mongo:3.4 1>/dev/null
 
-echo 'Mongo running\n'
+echo Mongo running
