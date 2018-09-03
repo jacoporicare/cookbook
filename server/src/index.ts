6ce81cf402a065b2b16fa9ec97be8e08d147dc25
Jakub Řičař <jakub.ricar@gmail.com>
2018-09-03 21:21:43 +0200

    Upgrade server packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 (mongoose as any).Promise = global.Promise;
 mongoose.connect(
   config.mongo.uri,
-  { useMongoClient: true },
+  { useNewUrlParser: true },
 );
 mongoose.connection.on('error', () => {
   throw new Error(`Unable to connect to database at ${config.mongo.uri}`);
