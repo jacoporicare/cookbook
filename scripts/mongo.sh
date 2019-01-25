1394fb275e3a284d8e9b69a1e34ed1222e120d2c
Jakub Řičař <jakub.ricar@gmail.com>
2019-01-25 23:04:02 +0100

    Upgrade Mongo to 4.0

--- scripts/mongo.sh
+++ scripts/mongo.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-CONTAINER_NAME=mongo
-DATA_SRC=~/dev/private/mongo_data
+CONTAINER_NAME=cookbook_mongo
+DATA_SRC=~/dev/private/mongo_data/cookbook
 DATA_DST=/data/db
 
 docker start $CONTAINER_NAME 1>/dev/null 2>&1 ||
@@ -11,6 +11,6 @@ docker run -d \
 	--name $CONTAINER_NAME \
 	-p 27017:27017 \
 	--mount type=bind,src=$DATA_SRC,dst=$DATA_DST \
-	mongo:3.4 1>/dev/null
+	mongo:4.0 1>/dev/null
 
 echo Mongo running
