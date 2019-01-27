8cacba8b5ca12d8203fc5713a79f89582caddc9c
Jakub Řičař <jakub.ricar@gmail.com>
2019-01-27 10:56:41 +0100

    Mongoose - retry connect on error or disconnect

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,26 +3,14 @@ import * as cors from 'cors';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
-import * as mongoose from 'mongoose';
 
+import * as db from './db';
 import config from './config';
 import routes from './routes';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-// tslint:disable-next-line no-any
-(mongoose as any).Promise = global.Promise;
-mongoose.connect(
-  config.mongo.uri,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-);
-mongoose.connection.on('error', () => {
-  throw new Error(`Unable to connect to database at ${config.mongo.uri}`);
-});
+db.connect();
 
 const app = express();
 
