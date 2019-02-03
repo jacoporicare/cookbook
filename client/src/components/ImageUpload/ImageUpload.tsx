c006af6bf5e02293cc5e16a2e7a5ab6e9ce9106c
Jakub Řičař <jakub.ricar@gmail.com>
2019-02-03 23:07:29 +0100

    Upgrade packages + Dropzone

--- client/src/components/ImageUpload/ImageUpload.tsx
+++ client/src/components/ImageUpload/ImageUpload.tsx
@@ -24,16 +24,6 @@ const text = css`
   z-index: 1;
 `;
 
-const Wrapper = styled('div')`
-  float: right;
-  margin-left: 15px;
-  position: relative;
-
-  &:hover .${text} {
-    z-index: 3;
-  }
-`;
-
 const Preview = styled('div')`
   position: absolute;
   width: 200px;
@@ -43,6 +33,43 @@ const Preview = styled('div')`
   border-radius: 4px;
   z-index: 2;
 `;
+
+type DropzonePlaceProps = {
+  isDragActive: boolean;
+  isDragAccept: boolean;
+  isDragReject: boolean;
+};
+const DropzonePlace = styled('div')<DropzonePlaceProps>(props => [
+  {
+    float: 'right',
+    marginLeft: '15px',
+    position: 'relative',
+    width: '204px',
+    height: '204px',
+    borderWidth: '2px',
+    borderColor: 'rgb(102, 102, 102)',
+    borderStyle: 'dashed',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    [`&:hover .${text}`]: {
+      zIndex: 3,
+    },
+  },
+  props.isDragActive && {
+    [`.${text}`]: {
+      zIndex: 3,
+    },
+  },
+  props.isDragAccept && {
+    borderStyle: 'solid',
+    borderColor: 'rgb(63, 195, 0)',
+  },
+  props.isDragReject && {
+    borderStyle: 'solid',
+    borderColor: 'rgb(195, 31, 31)',
+  },
+]);
 
 export class ImageUpload extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -79,34 +106,20 @@ export class ImageUpload extends React.Component<Props, State> {
     const src = this.state.image || this.props.imageUrl;
 
     return (
-      <Wrapper>
-        <Dropzone
-          multiple={false}
-          onDrop={this.handleDrop}
-          accept="image/*"
-          className={css`
-            width: 204px;
-            height: 204px;
-            border-width: 2px;
-            border-color: rgb(102, 102, 102);
-            border-style: dashed;
-            border-radius: 4px;
-            cursor: pointer;
-            overflow: hidden;
-          `}
-          acceptClassName={css`
-            border-style: solid;
-            border-color: rgb(63, 195, 0);
-          `}
-          rejectClassName={css`
-            border-style: solid;
-            border-color: rgb(195, 31, 31);
-          `}
-        >
-          {src && <Preview style={{ backgroundImage: `url(${src})` }} />}
-          <div className={text}>Klikni nebo sem přetáhni obrázek</div>
-        </Dropzone>
-      </Wrapper>
+      <Dropzone multiple={false} onDrop={this.handleDrop} accept="image/*">
+        {({ getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject }) => (
+          <DropzonePlace
+            {...getRootProps({ refKey: 'innerRef' })}
+            isDragActive={isDragActive}
+            isDragAccept={isDragAccept}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()} />
+            {src && <Preview style={{ backgroundImage: `url(${src})` }} />}
+            <div className={text}>Klikni nebo sem přetáhni obrázek</div>
+          </DropzonePlace>
+        )}
+      </Dropzone>
     );
   }
 }
