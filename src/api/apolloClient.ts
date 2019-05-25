693460b802f64ffb8a058a08142f8105177ea233
Jakub Řičař <jakub.ricar@gmail.com>
2019-05-25 11:56:50 +0200

    Uninstall http batch link

--- src/api/apolloClient.ts
+++ src/api/apolloClient.ts
@@ -1,7 +1,6 @@
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
-// import { BatchHttpLink } from 'apollo-link-batch-http';
 import { createUploadLink } from 'apollo-upload-client';
 import { setContext } from 'apollo-link-context';
 import { onError } from 'apollo-link-error';
