2872c97f7839a8ecf23560b4d31e434c6873874a
Jakub Řičař <jakub.ricar@gmail.com>
2020-12-08 09:56:39 +0100

    RecipeList to Material

--- src/components/RecipeList/RecipeListItem.tsx
+++ src/components/RecipeList/RecipeListItem.tsx
@@ -1,12 +1,19 @@
-import { Box, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core';
 import Link from 'next/link';
 import React from 'react';
 import { LazyImage } from 'react-lazy-images';
 
 import { RecipeBaseFragment } from '../../generated/graphql';
 import useSupportsWebP from '../../hooks/useSupportsWebP';
-import { colors } from '../../styles/colors';
-import RecipeInfo from '../RecipeInfo/RecipeInfo';
 
 type Props = {
   recipe: RecipeBaseFragment;
@@ -15,11 +22,13 @@ type Props = {
 const useStyles = makeStyles((_theme: Theme) =>
   createStyles({
     title: {
-      margin: 0,
-      fontWeight: 400,
       whiteSpace: 'nowrap',
       overflow: 'hidden',
       textOverflow: 'ellipsis',
+    },
+    media: {
+      height: 0,
+      paddingTop: '75%', // 4:3
     },
   }),
 );
@@ -28,72 +37,32 @@ function RecipeListItem({ recipe }: Props) {
   const classes = useStyles();
   const supportsWebP = useSupportsWebP();
 
-  const { slug, title, preparationTime, sideDish, image } = recipe;
+  const { slug, image } = recipe;
   const thumbUrl = supportsWebP ? image?.thumbWebPUrl : image?.thumbUrl;
   const placeholderUrl = `/assets/food-placeholder.${supportsWebP ? 'webp' : 'png'}`;
   const imageUrl = thumbUrl || placeholderUrl;
 
   return (
-    <>
-      <Link as={`/recept/${slug}`} href="/recept/[slug]">
-        <a className="link">
+    <Card>
+      <Link as={`/recept/${slug}`} href="/recept/[slug]" passHref>
+        <CardActionArea>
           <LazyImage
             actual={({ imageProps }) => (
-              <div className="image" style={{ backgroundImage: `url(${imageProps.src})` }} />
+              <CardMedia className={classes.media} image={imageProps.src} />
             )}
             placeholder={({ ref }) => (
-              <div
-                ref={ref}
-                className="image"
-                style={{ backgroundImage: `url('${placeholderUrl}')` }}
-              />
+              <CardMedia ref={ref} className={classes.media} image={placeholderUrl} />
             )}
             src={imageUrl}
           />
-          <div className="overlay">
-            <Typography className={classes.title} component="h3" variant="h5">
-              {title}
+          <CardContent>
+            <Typography className={classes.title} component="h2" variant="h5">
+              {recipe.title}
             </Typography>
-            <Box color={colors.gray200}>
-              <RecipeInfo
-                placeholder="žádné údaje"
-                preparationTime={preparationTime}
-                sideDish={sideDish}
-              />
-            </Box>
-          </div>
-        </a>
+          </CardContent>
+        </CardActionArea>
       </Link>
-      <style jsx>{`
-        .link {
-          display: block;
-          color: ${colors.gray900};
-          text-decoration: none;
-          position: relative;
-        }
-
-        .link:hover .overlay {
-          background-color: rgba(0, 0, 0, 0.6);
-        }
-
-        .overlay {
-          color: ${colors.white};
-          background-color: rgba(0, 0, 0, 0.4);
-          position: absolute;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          padding: 8px;
-        }
-
-        .image {
-          height: 0;
-          padding-top: 75%;
-          background-size: cover;
-          background-position: center center;
-        }
-      `}</style>
-    </>
+    </Card>
   );
 }
 
