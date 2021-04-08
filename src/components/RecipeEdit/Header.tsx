4c079da9fcbee3a71a50a3b6428c9c84beb47c9c
Jakub Řičař <jakub.ricar@gmail.com>
2021-04-08 15:12:56 +0200

    FAB on edit page always visible

--- src/components/RecipeEdit/Header.tsx
+++ src/components/RecipeEdit/Header.tsx
@@ -1,8 +1,7 @@
-import { CircularProgress, colors, Fab, makeStyles, Zoom } from '@material-ui/core';
+import { CircularProgress, colors, Fab, makeStyles } from '@material-ui/core';
 import { Save } from '@material-ui/icons';
 import React from 'react';
 
-import useHideOnScroll from '../../hooks/useHideOnScroll';
 import FabContainer from '../common/FabContainer';
 import PageHeading from '../common/PageHeading';
 
@@ -25,23 +24,20 @@ const useStyles = makeStyles({
 
 function Header({ title, isNew, isSaving, changed }: Props) {
   const classes = useStyles();
-  const fabHidden = useHideOnScroll();
 
   return (
     <>
       <PageHeading>{title || (isNew ? 'Nový recept' : ' receptu')}</PageHeading>
       <FabContainer>
-        <Zoom in={!fabHidden}>
-          <Fab
-            aria-label="Uložit"
-            color="primary"
-            disabled={!title || isSaving || !changed}
-            type="submit"
-          >
-            <Save />
-            {isSaving && <CircularProgress className={classes.fabProgress} size={68} />}
-          </Fab>
-        </Zoom>
+        <Fab
+          aria-label="Uložit"
+          color="primary"
+          disabled={!title || isSaving || !changed}
+          type="submit"
+        >
+          <Save />
+          {isSaving && <CircularProgress className={classes.fabProgress} size={68} />}
+        </Fab>
       </FabContainer>
     </>
   );
