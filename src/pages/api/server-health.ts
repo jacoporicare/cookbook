9e2fa1ec73b302bf77b14f7e0637c99886726b16
Jakub Řičař <jakub.ricar@gmail.com>
2021-04-08 12:13:46 +0200

    Upgrade packages

--- src/pages/api/server-health.ts
+++ src/pages/api/server-health.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default (_req: NextApiRequest, res: NextApiResponse) => {
+const serverHealth = (_req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Content-Type', 'application/health+json');
   res.status(200).json({ status: 'pass' });
 };
+
+export default serverHealth;
