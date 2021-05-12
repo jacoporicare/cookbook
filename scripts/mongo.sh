1faf0dfa93b1653af81a797e7522092287dc2075
Jakub Řičař <jakub.ricar@gmail.com>
2021-05-12 08:40:21 +0200

    Add debug Docker setup

--- scripts/mongo.sh
+++ scripts/mongo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-docker compose -f docker-compose.mongo.yml -p cookbook-mongo up -d
+[[ $1 = start ]] && docker compose -f docker-compose.mongo.yml -p cookbook-mongo up -d
+[[ $1 = stop ]] && docker compose -f docker-compose.mongo.yml -p cookbook-mongo down
