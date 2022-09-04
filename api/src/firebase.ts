06d5db57e90dfa987f41ce77d5dd57c5ca0cd0ad
Jakub Řičař <jakub.ricar@gmail.com>
2022-09-04 23:00:43 +0200

    Use Firebase credentials from env

--- api/src/firebase.ts
+++ api/src/firebase.ts
@@ -1,7 +1,9 @@
 import { cert, initializeApp } from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
 
-import serviceAccount from './zradelnik-firebase-adminsdk.json';
+const serviceAccount = process.env.FIREBASE_ADMIN_SDK
+  ? JSON.parse(process.env.FIREBASE_ADMIN_SDK)
+  : require('./zradelnik-firebase-adminsdk.json');
 
 initializeApp({
   credential: cert({
