819bf4dedda5e845007527ef29ac858c7c605f73
Jakub Řičař <jakub.ricar@gmail.com>
2022-09-06 08:10:55 +0200

    GitHub Actions

--- api/src/firebase.ts
+++ api/src/firebase.ts
@@ -1,16 +1,26 @@
 import { cert, initializeApp } from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
+import fs from 'fs-extra';
 
-const serviceAccount = process.env.FIREBASE_ADMIN_SDK
-  ? JSON.parse(process.env.FIREBASE_ADMIN_SDK)
-  : require('./zradelnik-firebase-adminsdk.json');
+import logger from './logger';
 
-initializeApp({
-  credential: cert({
-    projectId: serviceAccount.project_id,
-    privateKey: serviceAccount.private_key,
-    clientEmail: serviceAccount.client_email,
-  }),
-});
+try {
+  const serviceAccount = process.env.FIREBASE_ADMIN_SDK_PATH
+    ? JSON.parse(fs.readFileSync(process.env.FIREBASE_ADMIN_SDK_PATH).toString())
+    : require('./zradelnik-firebase-adminsdk.json');
+
+  initializeApp({
+    credential: cert({
+      projectId: serviceAccount.project_id,
+      privateKey: serviceAccount.private_key,
+      clientEmail: serviceAccount.client_email,
+    }),
+  });
+} catch (e) {
+  logger.warn(
+    'Firebase config not found (most probably, see error below).\nFor local development you have to download the Admin SDK JSON config file from Firebase console and place it to `api/src/zradelnik-firebase-adminsdk.json` (Git ignored).',
+  );
+  logger.error(e);
+}
 
 export const messaging = getMessaging();
