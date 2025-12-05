bf7dec1dc1fc6975923a08e6732eeaa9bfb9efb6
Jakub Řičař <jakub.ricar@gmail.com>
2025-12-05 20:50:09 +0100

    Update to Node 24

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -1,23 +1,10 @@
-import { Box, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 
 function Footer() {
   return (
     <Grid alignItems="center" container>
       <Grid item xs>
         © {new Date().getFullYear()} · Žrádelník
-      </Grid>
-      <Grid
-        component="a"
-        href="https://play.google.com/store/apps/details?id=cz.jakubricar.zradelnik&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
-        sx={{ height: '3rem' }}
-        item
-      >
-        <Box
-          alt="Nyní na Google Play"
-          component="img"
-          src="https://play.google.com/intl/en_us/badges/static/images/badges/cs_badge_web_generic.png"
-          sx={{ height: '3rem' }}
-        />
       </Grid>
     </Grid>
   );
